3d47e09baadd0eba1774f32bf8d8fd3d39a938b5
djholics2012 <zjshzjsh123@gmail.com>
2016-03-18 01:48:15 +0900

    물체가 ImageTarget으로 향하도록
    일단 이거까진 되었군.

---
+++ ARea_Game/Assets/Script/Enemy/Enemy_Move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Move : MonoBehaviour {
+
+    public float speed = 0.000001f;
+    public GameObject Target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //StartCoroutine(MoveToTarget());
+    }
+	
+    void Update()
+    {
+        Vector3 dir = Target.transform.position - transform.position;
+        dir.y = 0f;
+
+        dir.Normalize( );
+
+        transform.position += dir * speed;
+    }
+
+    /// <summary>
+    /*    IEnumerator MoveToTarget()
+    {
+        Vector3 dir = Target.transform.position - transform.position;
+    dir.y = 0f;
+
+        dir.Normalize( );
+
+        transform.position += dir * speed * Time.deltaTime;
+        
+        yield return null;
+    }*/
+    /// </summary>
+    /// <returns></returns>
+
+}
