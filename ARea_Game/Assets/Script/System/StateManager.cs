5e059550e3a17f0be0d8c65a0a2a2a4281d84ec0
djholics2012 <zjshzjsh123@gmail.com>
2016-03-21 22:25:08 +0900

    State&CharacterBase 구축
    아카데미에서 해왔던 것처럼 싱글톤으로 구축시킴.
    우선은 구현이 제일이라 여겨 Update와 Coroutine 의 동시 활용도 배제하면서 진행하였음.

---
+++ ARea_Game/Assets/Script/System/StateManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// 상태 관리 클래스. State의 변경을 관리하는 클래스이다. Ally, Enemy 모두 관리하는 클래스
+/// </summary>
+/// 
+
+public class StateManager<T>
+{
+    private T _owner; // 형식의 일반화
+    private Dictionary<int, State<T>> _stateList = new Dictionary<int, State<T>>();
+    /// <summary>
+    /// C#의 Dictionary 클래스이다.<>안의 값은 키의 값과 컬렉션을 나타닌다.
+    /// </summary>
+    private State<T> _curState = null; // 현재 state값
+    private CHARACTER_STATE _curStateID = CHARACTER_STATE.IDLE;
+
+    public CHARACTER_STATE CurStateID { get { return _curStateID; } }
+    public StateManager(T owner) { _owner = owner; }
+
+    public void AddState(int stateID, State<T> state) { _stateList.Add(stateID, state); }
+    public void ChangeState(int stateID)
+    {
+        if(!_stateList.ContainsKey(stateID))
+        {
+            Debug.LogError("State not found.");
+            return;
+        }
+
+        State<T> newState = _stateList[stateID];
+
+        if (newState == null)   return;
+        if (_curState == null)  return;
+        if (_curState != null)  _curState.Exit(_owner);
+
+        newState.Enter(_owner);
+
+        _curState = newState;
+        _curStateID = (CHARACTER_STATE)stateID;
+    }
+
+    public void Update()
+    {
+        if (_curState == null) return;
+        _curState.Update(_owner);
+    }
+
+    public void FixedUpdate()
+    {
+        if (_curState == null) return;
+        _curState.FixedUpdate(_owner);
+    }
+}
